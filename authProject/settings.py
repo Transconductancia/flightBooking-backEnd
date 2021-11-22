a514040e3d95971d6bb547535595ddf5e1bee166
transconductancia <carlos.rodridelghams@gmail.com>
2021-11-22 16:57:38 -0500

    fixed cretentials

--- authProject/settings.py
+++ authProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ys&)sxolev7gv904058fc1kma48id(a&!v6yfcyb2yb2qvriji'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -108,8 +109,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd4qalhrv358jvj',
         'USER':'rixtnxzlfnvyst',
-        'PASSWORD':'612e38d86400b5d1a6e499b28710a64ced18cb49b8bc016d3b538b691ece56d1',
-        'HOST':'ec2-23-23-162-138.compute-1.amazonaws.com',
+        'PASSWORD': config("PASSWORD"),
+        'HOST': config("HOST"),
         'PORT':'5432',
     }
 }
